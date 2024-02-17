ee31122a10fb8dc1d9c6f201ece81a5414f568dd
MAbdulahMunir <rana.abdullah4844@gmail.com>
2024-02-17 17:13:07 +0500

    Fixing file name casing

--- src/components/footerLinks/FooterLinks.tsx
+++ src/components/footerLinks/FooterLinks.tsx
@@ -1,10 +1,7 @@
 import { Link } from "react-router-dom";
 import { FooterLinksList } from "../../constants/FooterLink";
-import { useFooterLinks } from "./useFooterLinks";
-import ArrowUp from "../../assets/images/nav/Arrow.png";
 
 const FooterLinks = () => {
-  const { isFooterOpen, toggleFooter } = useFooterLinks();
   return (
     <div className="block md:flex flex-wrap mx-auto">
       {FooterLinksList.map((link) => {
@@ -14,29 +11,40 @@ const FooterLinks = () => {
             <div>
               <div className="inline-flex md:hidden flex-end pe-1">
                 <button
-                  onClick={toggleFooter}
                   title="Button"
                   type="button"
                   className="inline-flex items-center  text-sm text-white md:hidden font-bold"
                   aria-controls="footer"
-                  aria-expanded={isFooterOpen ? "true" : "false"}
                 >
-                  {link.title}
-                  <img src={ArrowUp} alt="" />
+                  {" "}
+                  <details>
+                    <summary>{link.title}</summary>
+                    {link.links.map((link) => {
+                      return (
+                        <ul>
+                          <li>
+                            <div className="mt-[11px] text-[13px] leading-none">
+                              <Link to={"/"}>{link.title}</Link>
+                            </div>
+                          </li>
+                        </ul>
+                      );
+                    })}
+                  </details>
                 </button>
               </div>
               <div
-                className={`${
-                  isFooterOpen ? "block" : "hidden"
-                } w-full md:flex md:w-auto lg:w-auto md:order-1`}
+                className={` w-full md:flex md:w-auto lg:w-auto md:order-1`}
                 id="footer"
               >
-                <div className=" flex flex-wrap md:p-0 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-                  <div className="me-[19px]">
+                <div className="  flex-wrap md:p-0 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  hidden md:block">
+                  <div className="me-[19px] ">
                     <ul className="text-white ">
                       <li>
-                        <div className="text-sm font-['Arial'] uppercase leading-snug hidden md:block">
-                          <Link to={"/"} className="font-bold">{link.title}</Link>
+                        <div className="text-sm font-['Arial'] uppercase leading-snug">
+                          <Link to={"/"} className="font-bold">
+                            {link.title}
+                          </Link>
                         </div>
                       </li>
                       <li>
