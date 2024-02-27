80cd8b4f9b7bfe55cd861464c3df7fc8da559049
MAbdulahMunir <rana.abdullah4844@gmail.com>
2024-02-27 13:06:19 +0500

    Fixing file name

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
-    screens: {
-      sm: "640px",
-
-      md: "768px",
-
-      lg: "1024px",
-
-      xl: "1280px",
-
-      "2xl": "1536px",
-    },
     extend: {
       colors: {
-        constPurple:"#6504b5",
-        customPrime: '#410078',
+        constPurple: "#6504b5",
+        customPrime: "#410078",
       },
     },
   },
