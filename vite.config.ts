f47dfe0cd7500f461e2782493df83c402a3cf8b2
MAbdulahMunir <rana.abdullah4844@gmail.com>
2024-03-11 12:11:06 +0500

    Fixed all issuses

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [
     react(),
     EnvironmentPlugin({
-      VITE_APP_API_KEY: process.env.REACT_APP_API_KEY as  string | undefined,
-      VITE_APP_API_SECRET: process.env.REACT_APP_API_SECRET as  string | undefined,
+      VITE_APP_API_KEY: process.env.REACT_APP_API_KEY || "x6ty0NnOjoeXPkrGRLoMTXffnuhSLcsinNci8DV9bl4IlF4M9b",
+      VITE_APP_API_SECRET: process.env.REACT_APP_API_SECRET || "mBD6eu0JvZ7bCYfCnsQRtBZ9hwobQEM3Q2UG7oie",
     }),
   ],
 });
